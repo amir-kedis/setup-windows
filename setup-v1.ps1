2253f580a14a46ecacf3a0012b9894c389faf73c
amir-kedis <amir.kedis@gmail.com>
2024-10-26 20:22:45 +0300

    feat: set-default browser

--- setup-v1.ps1
+++ setup-v1.ps1
@@ -7,4 +7,8 @@ Start-Process -FilePath "$env:TEMP\vscode-installer.exe" -ArgumentList "/silent 
 
 Write-Host "Visual Studio Code installed successfully!"
 
-
+Write-Host "Setting default browser to Microsoft Edge..."
+'http', 'https' | ForEach-Object {
+  $regPath = "HKCU:\Software\Microsoft\Windows\Shell\Associations\UrlAssociations\$_\UserChoice"
+  Set-ItemProperty $regPath ProgId MSEdgeHTM
+}
